1d8187f78356ee9d4726b48e0cc7f1ed09365158
mohammad- <m.k.bharmal@gmail.com>
2014-07-15 19:50:54 +0900

    adding ex1.py

--- day1/day1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-from sys import argv
-script, runner = argv
-print "Hi %s,\n I am mohammad" % runner
-you=raw_input("How are you? > ")
-print "So you are %r" % you
-
---
+++ day1/ex1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+print "Hi How are you"
+print "this is simple printing in python"
+print "each python program is made up of modules"
+print "each module is made of statments"
